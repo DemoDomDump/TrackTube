7f156aa2efd6044b35519e52cd982453047075c1
DemoDomDump <azzip02@gmail.com>
2020-10-18 23:44:40 +0300

    Start top comments page

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,58 @@
 <template>
-  <v-container fluid class="pa-8">
-    <v-row justify="center" class="mb-8">
+  <v-container fluid>
+    <v-row justify="center" class="my-8">
       <div class="text-h2 text-wrap text-center">Hello, DemoDom</div>
     </v-row>
-    <v-row align="center" justify="center" class="grey lighten-5 mb-8">
-      <v-card v-for="n in 3" :key="n" class="ma-3 pa-6" max-width="400" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-          </v-list-item-content>
+    <v-row align="center" justify="center" class="grey lighten-1 py-8">
+      <v-card v-for="n in 3" :key="n" class="ma-4" rounded="0">
+        <v-img
+          src="https://img.youtube.com/vi/iNVHHgymrg8/maxresdefault.jpg"
+          :aspect-ratio="16 / 9"
+          width="280"
+        ></v-img>
 
-          <v-list-item-avatar tile width="160" height="90" color="grey"></v-list-item-avatar>
-        </v-list-item>
+        <v-card-title>Top western road trips</v-card-title>
+
+        <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
       </v-card>
     </v-row>
-    <v-row class="grey lighten-3 mb-8">
+    <v-row class="grey lighten-3 py-8">
       <v-col cols="12">
-        <v-card class="ma-3 pa-6" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">OVERLINE</div>
-              <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-              <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-            </v-list-item-content>
+        <v-card class="card" tile flat router>
+          <v-row no-gutters>
+            <v-col cols="12" md="8">
+              <div class="ma-5">
+                <v-card-title class="pl-2 pt-0 subtitle-1 font-weight-bold">
+                  Top western road trips
+                </v-card-title>
 
-            <v-list-item-avatar tile width="240" height="135" color="grey"></v-list-item-avatar>
-          </v-list-item>
+                <v-card-subtitle class="pl-2 pb-0">
+                  1,000 miles of wonder
+                  <v-icon>mdi-circle-small</v-icon>9.6k
+                  views<v-icon>mdi-circle-small</v-icon>6 hours ago
+                </v-card-subtitle>
+                <v-card-subtitle class="pl-2 pt-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Molestiae exercitationem minus in asperiores. Dolorem dicta ea
+                  praesentium, repellendus ipsam harum optio, delectus facilis
+                  ratione quam quaerat a architecto hic reprehenderit.
+                </v-card-subtitle>
+              </div>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-img
+                class="ma-5"
+                contain
+                max-height="200"
+                src="https://img.youtube.com/vi/iNVHHgymrg8/maxresdefault.jpg"
+              >
+              </v-img>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
-    <v-row class="grey lighten-1 mb-8">
+    <v-row class="grey lighten-1 py-8 px-4">
       <v-col cols="12" md="4" v-for="col in 3" :key="col">
         <v-card class="mx-auto">
           <v-card-title class="white--text grey darken-1">
@@ -44,16 +66,24 @@
 
           <v-list three-line>
             <template v-for="(item, index) in items">
-              <v-divider v-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+              <v-divider
+                v-if="item.divider"
+                :key="index"
+                :inset="item.inset"
+              ></v-divider>
 
               <v-list-item :key="item.title" @click>
                 <v-list-item-content>
                   <v-list-item-title v-html="item.title"></v-list-item-title>
-                  <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                  <v-list-item-subtitle
+                    v-html="item.subtitle"
+                  ></v-list-item-subtitle>
                 </v-list-item-content>
 
                 <v-list-item-avatar tile width="160" height="90">
-                  <v-img :src="item.avatar"></v-img>
+                  <v-img
+                    src="https://img.youtube.com/vi/iNVHHgymrg8/maxresdefault.jpg"
+                  ></v-img>
                 </v-list-item-avatar>
               </v-list-item>
             </template>
@@ -64,7 +94,6 @@
   </v-container>
 </template>
 
-
 <script>
 export default {
   data: () => ({
@@ -73,34 +102,34 @@ export default {
         avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
         title: "Brunch this weekend?",
         subtitle:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
       },
       {
         avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
         title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
         subtitle:
-          "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+          "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
       },
       {
         avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
         title: "Oui oui",
         subtitle:
-          "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+          "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
       },
       {
         avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
         title: "Birthday gift",
         subtitle:
-          "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+          "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
       },
       {
         avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
         title: "Recipe to try",
         subtitle:
-          "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-      },
-    ],
-  }),
+          "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+      }
+    ]
+  })
 };
 </script>
 <style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,73 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-row justify="center" class="my-8">
+      <div class="text-h2 text-wrap text-center">Hello DemoDom</div>
+    </v-row>
+    <v-row class="grey lighten-1 py-8 px-4">
+      <v-col cols="12">
+        <v-text-field
+        flat
+        hide-details
+        append-icon="mdi-magnify"
+        placeholder="Search"
+        outlined
+        dense
+        v-model="searchText"
+      ></v-text-field>
+        <v-card class="mx-auto mt-4">
+          <v-card-title class="white--text grey darken-1">
+            Comments
+            <v-spacer></v-spacer>
+
+            <v-btn color="white" class="text--primary" fab small>
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-list three-line>
+          <template v-for="(item, index) in comments">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              v-text="item.header"
+            ></v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-item
+              v-else
+              :key="item.title"
+            >
+              <v-list-item-avatar>
+                <v-img :src="item.avatar"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    searchText: "",
+  }),
+  computed: {
+    comments() {
+      return this.$store.getters.userComments;
+    }
+  },
+};
+</script>
